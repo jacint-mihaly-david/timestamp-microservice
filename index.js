fb8e529e0d81f24b4a83954b5a92f655251a08ae
jacint-mihaly-david <107406590+jacint-mihaly-david@users.noreply.github.com>
2023-09-10 22:36:11 +0000

    init : [FCC-BE-1] - finished project

--- index.js
+++ index.js
@@ -8,25 +8,45 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", (req, res) => {
+  let { date } = req.params;
+  console.log('req.params.date:', date);
+  // if param :date isn't provided assign now to date
+  if (!date) {
+    date = Date.now();
+  }
+  // if format is seconds since epoch convert type into number before constructing Date obj
+  if (Number(date)) {
+    date = Number(date);
+  }
+  // if date param is invalid
+  if (isNaN(new Date(date))) {
+    const response = { "error": "Invalid Date" };
+    console.log('response:', response);
+    console.log();
+    res.json(response);
+    return;
+  }
+  const unixDate = new Date(date).getTime();
+  const utcDate = new Date(date).toUTCString();
+  const response = { "unix": unixDate, "utc": utcDate };
+  console.log('response:', response);
+  console.log();
+  res.json(response);
+  return;
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port + '\n');
 });
